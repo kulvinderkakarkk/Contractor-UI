0349e896285e9e99b8e35257d48c9c42e5e550c2
kulvinder kakar <kulvinderkakar14@gmail.com>
2021-09-03 14:44:16 +1000

    modified apollo server to handle file uploads

--- index.js
+++ index.js
@@ -1,17 +1,49 @@
-const gql=require('graphql-tag')
+import { GraphQLModule } from '@graphql-modules/core';
+import gql from 'graphql-tag';
+const path=require('path')
+const express=require('express')
 const mongoose=require('mongoose')
-const {ApolloServer} =require('apollo-server')
-const {MONGODB} =require('./config')
-
+const {graphqlHTTP}=require('express-graphql')
 const typeDefs = require('./graphql/typeDefs')
+const {MONGODB} =require('./config')
 const resolvers = require('./graphql/resolvers')
+const app = express();
+var cors = require('cors')
+app.use(cors())
+const {GraphQLUpload, graphqlUploadExpress} = require('graphql-upload');
+const { schema } = new GraphQLModule({
+    typeDefs: typeDefs,
+  resolvers: resolvers
+});
+const PORT = process.env.PORT || 4000
 
-const PORT = process.env.PORT || 5000
-const server = new ApolloServer({typeDefs, resolvers, context:({req})=>({req})})
+app.use(
+  '/graphql',
+  graphqlUploadExpress({ maxFileSize: 10000000, maxFiles: 10 }),
+  graphqlHTTP({ schema, graphiql: true })
+);
 
+app.get('/images/:name',(req,res) => {
+  
+  var options = {
+    root: path.join(__dirname, 'images'),
+    dotfiles: 'deny',
+    headers: {
+      'x-timestamp': Date.now(),
+      'x-sent': true
+    }
+  }
+  var fileName = req.params.name
+  res.sendFile(fileName, options, function (err) {
+    if (err) {
+      next(err)
+    } 
+  })
+})
 mongoose.connect(MONGODB, {useNewUrlParser:true, useUnifiedTopology:true})
         .then(()=>{
-            server.listen({port:PORT})
+            app.listen(PORT);
+            console.log("🚀 Server started on",PORT)
         })
         .catch((err)=>{
             console.log("error connecting to MONGO server", err)
